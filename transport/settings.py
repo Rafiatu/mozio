fce1c89a4a10a8e91f43d2186891cc78fc441b30
Rafiatu <rafibella101@gmail.com>
2022-02-23 11:49:54 +0100

    update settings

--- transport/settings.py
+++ transport/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key(), cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
